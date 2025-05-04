7ff18953e8a8ef4f28aad6f46d6c2a4970ca5015
DhamodharanSP <gowthamdhamo24@gmail.com>
2025-05-04 20:37:57 +0530

    Feat: Designed Stack using Linked List

--- src/LinkedListProblems/DesignStack/LinkedListStack.java
+++ src/LinkedListProblems/DesignStack/LinkedListStack.java
@@ -1,4 +1,99 @@
 package LinkedListProblems.DesignStack;
 
-public class LinkedListStack {
+class ListNode
+{
+    private int data;
+    ListNode next;
+
+    ListNode(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+
+    public void setData(int data)
+    {
+        this.data = data;
+    }
+    public int getData()
+    {
+        return this.data;
+    }
+
+    public void setNext(ListNode next)
+    {
+        this.next = next;
+    }
+    public ListNode getNext()
+    {
+        return this.next;
+    }
+}
+
+public class LinkedListStack
+{
+    private int length;
+    ListNode top;
+
+    LinkedListStack()
+    {
+        this.length = 0;
+        this.top = null;
+    }
+
+    public int getLength()
+    {
+        return this.length;
+    }
+
+    public void push(int data)
+    {
+        ListNode node = new ListNode(data);
+        if(top == null)
+            this.top = node;
+        else
+        {
+            node.setNext(top);
+            this.top = node;
+        }
+        this.length++;
+    }
+
+    public int pop()
+    {
+        int data = -1;
+        if(top == null)
+        {
+            System.out.println("Stack is Empty! Pop() is not possible!");
+            return -1;
+        }
+        else
+        {
+            data = top.getData();
+            this.top = top.getNext();
+            this.length--;
+        }
+        return data;
+    }
+
+    public boolean isEmpty()
+    {
+        return top == null;
+    }
+
+    public void printStack()
+    {
+        if(top == null)
+        {
+            System.out.println("Stack is Empty... No elements to Print!");
+            return;
+        }
+        ListNode temp = top;
+        while(temp != null)
+        {
+            System.out.print(" |  "+ temp.getData() +"  | \n");
+            System.out.println(" _________ ");
+            temp = temp.getNext();
+        }
+    }
 }
--- src/LinkedListProblems/DesignStack/Main.java
+++ src/LinkedListProblems/DesignStack/Main.java
@@ -1,4 +1,33 @@
 package LinkedListProblems.DesignStack;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        LinkedListStack stack = new LinkedListStack();
+
+        System.out.print("Enter Number of elements to push into stack: ");
+        int n = in.nextInt();
+        System.out.println("Enter "+n+" elements: ");
+        for(int i=0;i<n;++i)
+        {
+            stack.push(in.nextInt());
+        }
+        stack.printStack();
+        System.out.println("Stack Length: " + stack.getLength());
+
+        System.out.println("After one pop()");
+        int data = stack.pop();
+        if(data!=-1)
+            System.out.println(data+" is poped!");
+        stack.printStack();
+        System.out.println("Stack Length: " + stack.getLength());
+
+        System.out.println("Stack Empty Status: "+ stack.isEmpty());
+
+
+    }
 }
