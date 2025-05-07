9c77d0437ebb17326fb6989d00089b27e724bae5
DhamodharanSP <gowthamdhamo24@gmail.com>
2025-05-07 11:41:35 +0530

    Feat: LinkedList Class created

--- src/Day25/LinkedList.java
+++ src/Day25/LinkedList.java
@@ -1,4 +1,48 @@
 package Day25;
 
-public class LinkedList {
+public class LinkedList
+{
+    private int length;
+    ListNode head;
+    LinkedList()
+    {
+        this.length = 0;
+        this.head = null;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+
+    public void add(int data)
+    {
+        ListNode node = new ListNode(data);
+        if(head == null)
+            head = node;
+        else
+        {
+            ListNode temp = head;
+            while(temp.next!=null)
+                temp = temp.next;
+            temp.next = node;
+        }
+        this.length++;
+    }
+
+    public void printList()
+    {
+        if(head == null)
+        {
+            System.out.println("Linked List is Empty... No elements to print!");
+            return;
+        }
+        ListNode temp = head;
+        while(temp.next!=null)
+        {
+            System.out.print(temp.data +" -> ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
 }
