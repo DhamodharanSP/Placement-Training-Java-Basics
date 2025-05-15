688570c12f706f6764b5fc20c48d8eaf1415884a
DhamodharanSP <gowthamdhamo24@gmail.com>
2025-05-15 05:51:31 +0530

    Feat: Doubly Linked List implementation

--- src/Day26/DoublyLinkedList.java
+++ src/Day26/DoublyLinkedList.java
@@ -1,4 +1,101 @@
 package Day26;
 
-public class DoublyLinkedList {
+class ListNode
+{
+    int data;
+    ListNode prev;
+    ListNode next;
+    ListNode(int data)
+    {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+public class DoublyLinkedList
+{
+    private  int length;
+    ListNode head;
+    ListNode tail;
+    DoublyLinkedList()
+    {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+
+    public int getLength()
+    {
+        return length;
+    }
+
+    public void insertFirst(int data)
+    {
+        ListNode node = new ListNode(data);
+        if(head == null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            node.next = head;
+            head.prev = node;
+            head = node;
+        }
+    }
+
+    public void insertLast(int data)
+    {
+        ListNode node = new ListNode(data);
+        if(head == null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            node.prev = tail;
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    public void printList()
+    {
+        if(head == null)
+        {
+            System.out.println("Doubly Linked List is Empty... No items to print!");
+            return;
+        }
+        ListNode temp = head;
+        while(temp.next!=null)
+        {
+            System.out.print(temp.data +" -> ");
+            temp = temp.next;
+        }
+        System.out.println(temp.data);
+    }
+
+    public void printList(int ch)
+    {
+        if(ch == -1)
+        {
+            if (tail == null) {
+                System.out.println("Doubly Linked List is Empty... No items to print!");
+                return;
+            }
+            ListNode temp = tail;
+            System.out.print(temp.data);
+            while (temp.prev != null) {
+                System.out.print(" <- " + temp.prev.data);
+                temp = temp.prev;
+            }
+        }
+        else
+            System.out.println("Invalid format!");
+    }
+
+
 }
--- src/Day26/Main.java
+++ src/Day26/Main.java
@@ -1,4 +1,23 @@
 package Day26;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        System.out.println("Enter elements to append the Doubly linked list... (or) Press -1 to stop getting input!");
+
+        while(true)
+        {
+            int n = in.nextInt();
+            if(n==-1) break;
+            list.insertLast(n);
+        }
+        list.printList();
+        list.printList(-1);
+    }
 }
